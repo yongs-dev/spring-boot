4c10c981f6a00460e1327439900f5acffff040f6
Ethan <yongseok993@gmail.com>
2023-08-01 13:54:38 +0900

    git init

---
+++ springboot_02/src/test/java/com/springboot/mark/MarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
