8b2a5e0c293329ba63d49fab73f42b0376b6100e
Ethan <yongseok993@gmail.com>
2023-08-02 14:15:08 +0900

    Spring Data JPA 활용

--- springboot_02/src/main/java/com/springboot/mark/course/CourseCommandLineRunner.java
+++ springboot_02/src/main/java/com/springboot/mark/course/CourseCommandLineRunner.java
@@ -1,6 +1,7 @@
 package com.springboot.mark.course;
 
 import com.springboot.mark.course.jpa.CourseJpaRepository;
+import com.springboot.mark.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,19 +12,25 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 //    @Autowired
 //    private CourseJdbcRepository repository;
 
+//    @Autowired
+//    private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Mark", "Ethan"));
-        repository.insert(new Course(2, "Mark2", "Ethan"));
-        repository.insert(new Course(3, "Mark3", "Ethan"));
+        repository.save(new Course(1, "Mark", "Ethan"));
+        repository.save(new Course(2, "Mark2", "Ethan"));
+        repository.save(new Course(3, "Mark3", "Ethan"));
 
-        repository.deleteById(1);
+        repository.deleteById(1L);
 
-        System.out.println(repository.findById(2).toString());
-        System.out.println(repository.findById(3).toString());
+        System.out.println(repository.findById(2L));
+        System.out.println(repository.findById(3L));
+
+        System.out.println(repository.findByAuthor("Ethan"));
+        System.out.println(repository.findByName("Mark2"));
     }
 }
---
+++ springboot_02/src/main/java/com/springboot/mark/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -0,0 +1,12 @@
+package com.springboot.mark.course.springdatajpa;
+
+import com.springboot.mark.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByAuthor(String author);
+    List<Course> findByName(String name);
+}
