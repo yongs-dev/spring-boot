4cb7349226f6cedef984b3ad9cfdcc84cbe648e5
Ethan <yongseok993@gmail.com>
2023-08-24 17:43:58 +0900

    Todo App RestAPI FrontEnd 연동

---
+++ springboot_todo/src/test/java/com/spring/mark/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mark.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
